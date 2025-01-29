2cbf0c2e8f1937d60190828fe265488e113276e7
Armando Canales <arfoca@gmail.com>
2025-01-29 16:24:03 -0500

    crea la funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let listaDeAmigos = [];
+
+function agregarAmigo() {
+   let amigo = document.getElementById('amigo').value;
+   console.log(amigo); 
+   listaDeAmigos.push(amigo);
+   console.log(listaDeAmigos);
+}
+
+
+let numeroMaximo = listaDeAmigos.length;
+
+function sortearAmigo() {
+    let numeroSorteado = Math.floor(Math.random()*numeroMaximo)+1;
+
+}
