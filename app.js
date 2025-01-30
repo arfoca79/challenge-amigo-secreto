9fbedbd5ce4444f3d08aedf56267dc70189d3aa8
Armando Canales <arfoca@gmail.com>
2025-01-30 15:42:19 -0500

    edita las funciones

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let listaDeAmigos = [];
+let numeroMaximo = 1;
+
+function asignarTextoElemento(texto) {
+    let elementoHTML = document.querySelector('#listaAmigos');
+    elementoHTML.innerHTML += `<div role="listitem">${texto}</div>`;
+}
+
+function limpiarListaNombres() {
+    let elementoHTML = document.querySelector('#listaAmigos');
+    elementoHTML.innerHTML = "";
+}
 
 function agregarAmigo() {
-   let amigo = document.getElementById('amigo').value;
-   console.log(amigo); 
-   listaDeAmigos.push(amigo);
-   console.log(listaDeAmigos);
+    let amigo = document.getElementById('amigo').value;
+    if (amigo == '') {
+        alert('Por favor ingrese un nombre válido');
+    } else {
+        listaDeAmigos.push(amigo);
+        asignarTextoElemento(amigo);
+        limpiarCaja();
+    }
 }
-
-
-let numeroMaximo = listaDeAmigos.length;
 
 function sortearAmigo() {
-    let numeroSorteado = Math.floor(Math.random()*numeroMaximo)+1;
-
+    numeroMaximo = listaDeAmigos.length;
+    let numeroSorteado = Math.floor((Math.random()*numeroMaximo));
+    limpiarListaNombres();
+    let listaResultado = document.querySelector('#resultado');
+    listaResultado.innerHTML = `El amigo sorteado es: ${listaDeAmigos[numeroSorteado]}`;
 }
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
+
